86ab329d677036ab341560a5e81e72cc671a73dd
fanyu.xhy <fanyu.xhy@taobao.com>
2014-10-23 14:32:21 +0800

    收尾工作

---
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,20 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// GCDWebServer
+#define COCOAPODS_POD_AVAILABLE_GCDWebServer
+#define COCOAPODS_VERSION_MAJOR_GCDWebServer 3
+#define COCOAPODS_VERSION_MINOR_GCDWebServer 0
+#define COCOAPODS_VERSION_PATCH_GCDWebServer 0
+
+// GCDWebServer/Core
+#define COCOAPODS_POD_AVAILABLE_GCDWebServer_Core
+#define COCOAPODS_VERSION_MAJOR_GCDWebServer_Core 3
+#define COCOAPODS_VERSION_MINOR_GCDWebServer_Core 0
+#define COCOAPODS_VERSION_PATCH_GCDWebServer_Core 0
+
---
+++ README.md
@@ -0,0 +1,23 @@
+# 简介
+
+MobileWebPageTest是用来测试移动网页性能的软件，它可以对页面的加载和渲染过程进行截屏，协助开发者分析出页面性能瓶颈。
+
+# 功能
+
+## 主界面
+![](http://gtms03.alicdn.com/tps/i3/TB1XHaNGFXXXXaXXXXXbIDTJVXX-974-1602.png)
+
+## 页面加载渲染过程截屏
+
+![](http://gtms02.alicdn.com/tps/i2/TB1nNqMGFXXXXaUXXXXbIDTJVXX-974-1602.png)
+
+## 测试结果
+
+![](http://gtms04.alicdn.com/tps/i4/TB1AqyNGFXXXXX_XXXXbIDTJVXX-974-1602.png)
+
+## 远程模式
+
+![](http://gtms01.alicdn.com/tps/i1/TB17XaNGFXXXXXTXXXXpTLOIFXX-2776-1778.png)
+
+
+
